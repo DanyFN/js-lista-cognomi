57ed4d4a9888827f34ce4ba26a2017f604e331ff
Daniele <danielefontana90@gmail.com>
2020-05-27 10:29:40 +0200

    ottimizzazione codice

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// . Chiedi all’utente il cognome, inseriscilo in un
+// Chiedi all’utente il cognome, inseriscilo in un
 // array con altri cognomi e stampa la lista
 // ordinata alfabeticamente. Scrivi a che
 // posizione della lista il nuovo utente si trova
@@ -8,6 +8,12 @@ var arrayCognomi = ["daultrey","joplin","hendrix","mercury","lennon","gilmur"];
 
 // inserisco un prompt per generare un cognome
 var cognome = prompt("inserisci cognome");
+
+// dico al programma di reinserire il cognome se non si scrive nulla o si scrive un numero
+while ((isNaN(parseInt(cognome))===false) || cognome.length == 0) {
+  cognome = prompt("ERRORE!! Inserisci di nuovo il tuo cognome");
+}
+
 //pusho all'intenro dell'array il mio prompt(cognome inserito)
 var cognomePush = arrayCognomi.push(cognome);
 
